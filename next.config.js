1b7749a5f4785e27b418898355a29c7a6a324950
Caleb Denio <cjdenio44@gmail.com>
2021-09-27 11:17:02 -0400

    chore(dev): API proxy in dev

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  async rewrites() {
+    if (process.env.NO_PROXY) return [];
+
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5000/api/:path*",
+      },
+    ];
+  },
+};
