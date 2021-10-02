e0c69e44d7af87fb248a4f04af66027673eabeb4
anirudhb <anirudhb@users.noreply.github.com>
2021-10-02 15:44:49 -0700

    cypress: add component testing infra

---
+++ cypress/plugins/index.js
@@ -0,0 +1,6 @@
+module.exports = (on, config) => {
+  if (config.testingType === "component") {
+    require("@cypress/react/plugins/next")(on, config);
+  }
+  return config;
+};
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import "cypress-react-selector";
 
 declare namespace Cypress {
   interface Chainable {
