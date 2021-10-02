b1f60128e231e1f3f4c4d0799aa3b27c7de93f8f
anirudhb <anirudhb@users.noreply.github.com>
2021-10-02 15:45:19 -0700

    cypress-ct: add test helpers

---
+++ src/lib/testHelpers.tsx
@@ -0,0 +1,9 @@
+import { mount } from "@cypress/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
+
+export function mountChakra(node: React.ReactNode) {
+  mount(<ChakraProvider theme={theme}>
+    {node}
+  </ChakraProvider>);
+}
