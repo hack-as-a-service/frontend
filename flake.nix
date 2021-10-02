351083718f16670f07890460b706d36c19cf37ac
anirudhb <anirudhb@users.noreply.github.com>
2021-10-02 15:42:13 -0700

    nix: add flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShell = pkgs.mkShell {
+      nativeBuildInputs = [
+        pkgs.yarn
+        pkgs.nodePackages.typescript-language-server
+      ];
+    };
+  });
+}
