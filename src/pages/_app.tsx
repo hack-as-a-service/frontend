af17ae8afd6950ac684d234b325b9c6ad9326c39
Caleb Denio <cjdenio44@gmail.com>
2021-10-30 21:51:34 -0400

    feat: Progress indicator for route changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,16 @@ import theme from "../theme";
 import { SWRConfig } from "swr";
 import fetchApi from "../lib/fetch";
 import { ChakraProvider } from "@chakra-ui/react";
-import "@hackclub/theme/fonts/reg-bold.css";
+import Router from "next/router";
+import NProgress from "nprogress";
 
+import "@hackclub/theme/fonts/reg-bold.css";
 import "../styles/globals.css";
+import "../styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
 	return (
