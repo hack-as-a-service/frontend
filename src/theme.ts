d861db3dd35547cdd068cce18fa27e55154ca7d7
Jason Antwi-Appah <hey@jasonaa.me>
2021-12-25 23:30:54 -0600

    chore: add some colors from @hackclub/theme

--- src/theme.ts
+++ src/theme.ts
@@ -7,6 +7,20 @@ export default extendTheme({
 	},
 	initialColorMode: "dark",
 	useSystemColorMode: true,
+	colors: {
+		darker: "#121217",
+		dark: "#17171d",
+		darkless: "#252429",
+		black: "#1f2d3d",
+		steel: "#273444",
+		slate: "#3c4858",
+		muted: "#8492a6",
+		smoke: "#e0e6ed",
+		snow: "#f9fafc",
+		elevated: "#ffffff",
+		sheet: "#f9fafc",
+		sunken: "#e0e6ed",
+	},
 	components: {
 		// Input: {
 		//   parts: ["field"],
